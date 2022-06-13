62c6d1b06d4dfdf4a239442202faa1e5e4494f07
Reanna Ishmael <rishmael@fordham.edu>
2022-06-12 21:56:14 -0400

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    let finalStringIndex = string.length - 1;
+    for (let i = finalStringIndex; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
