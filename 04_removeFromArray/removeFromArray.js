dcc6d22f57b2749c4eaf297b9121a82054377223
Reanna Ishmael <rishmael@fordham.edu>
2022-06-13 00:25:35 -0400

    Finish the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removableItems) {
+    let newArr = arr.filter(
+        (item) => !removableItems.includes(item)
+    )
+    return newArr;
 };
+
+// .includes() uses === comparison
+// will return false if items are not the same type
 
 // Do not edit below this line
 module.exports = removeFromArray;
