b6befb5bc68504d74cf776343310664c375b2eae
Reanna Ishmael <rishmael@fordham.edu>
2022-06-13 00:54:53 -0400

    Finish the sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
