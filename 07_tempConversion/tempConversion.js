3d170be9902055fbc841d2ee49b80819fef52826
Reanna Ishmael <rishmael@fordham.edu>
2022-06-14 00:36:19 -0400

    Finish the tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (5 / 9) * (fTemp - 32);
+  return Math.round(cTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = (9 / 5) * cTemp + 32;
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
